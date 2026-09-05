22b43c581115279b2cd03ee529c175c6aee7187a
Harrison <223957058+Harris0nn@users.noreply.github.com>
2026-09-05 10:43:47 +1000

    Updated numbers file, nearly finished

---
+++ 03_Numbers.py
@@ -0,0 +1,27 @@
+""""
+The numeric types in python are:
+- int ( x = 1 )
+- float ( y = 2.8 )
+- complex ( z = 1j )
+"""
+
+# Each numeric type will have their own function so there is no confliction
+
+def intFunc(): # An int or integer is a whole number, postive or negative but with no decimals and no maximum length
+    x = 7
+    y = 39293842948
+    z = 7333733
+
+    print("These are int numbers")
+    print(x, "is", type(x))
+    print(y, "is", type(x))
+    print(z, "is", type(x))
+intFunc()
+
+def floatFunc(): # A flot is a number, positive or negative that contains one or more decimals
+    x
+floatFunc()
+
+def complexFunc(): # A complex number is written with a "j" as the imaginary part
+    x
+complexFunc()
