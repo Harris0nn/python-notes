925d646aaadc5f5b539a654393825070ae654995
Harrison <223957058+Harris0nn@users.noreply.github.com>
2026-09-10 10:45:13 +1000

    Added strings file, not completed yet

---
+++ 05_strings.py
@@ -0,0 +1,21 @@
+# Strings (str) in python are surrounded by single or double quotation marks.
+
+print("double quotes")
+print('single quotes')
+
+print("You are 'one'") # Yoou can use quotes inside strings but they cannot be the same as the surrounding strings
+
+def strfunc(): # You can assign a string to a variable
+    a = "hello"
+    b = 'bye'
+
+    print(a, b)
+
+strfunc()
+
+c = """Lorem ipsum dolor sit amet,  
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua """ # You can use multiline string to a variable
+
+print(a)
